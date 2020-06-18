e2f973cd3c07e0ee864ace2b30954b630a9e8032
Alexander Karateev <administrator@gintr1k.space>
2020-06-18 12:54:54 +0300

    add dict converting to bytes
    delete python 3.5 classifier in setup.py

--- setup.py
+++ setup.py
@@ -12,7 +12,6 @@ classifiers = [
     'Development Status :: 3 - Alpha',
     'Programming Language :: Python',
     'Programming Language :: Python :: 3',
-    'Programming Language :: Python :: 3.5',
     'Programming Language :: Python :: 3.6',
     'Programming Language :: Python :: 3.7',
     'Programming Language :: Python :: 3.8',
