31d38a5d6d3f52ffb7c2a274da2821401e570f58
Albert Tugushev <albert@tugushev.ru>
2020-08-22 17:14:09 +0700

    Add type hints for ansq.utils module

--- ansq/utils.py
+++ ansq/utils.py
@@ -6,27 +6,27 @@ from decimal import Decimal
 from enum import Enum
 from functools import singledispatch
 from sys import version_info
-from typing import Optional, Tuple, Union
+from typing import Any, Optional, Tuple, Union
 from urllib.parse import urlsplit
 
 PY37 = version_info >= (3, 7)
 
 
 class JSONEncoder(json.JSONEncoder):
-    def default(self, obj) -> str:
+    def default(self, obj: Any) -> str:
         try:
             return convert_to_str(obj)
         except TypeError:
             return json.JSONEncoder.default(self, obj)
 
 
-def get_host_port(uri: str) -> Tuple[str, Optional[int]]:
+def get_host_port(uri: str) -> Tuple[Optional[str], Optional[int]]:
     """Get host and port from provided URI."""
     split_uri = urlsplit(uri)
     return split_uri.hostname, split_uri.port
 
 
-def validate_topic_channel_name(name: str):
+def validate_topic_channel_name(name: str) -> None:
     """Validate topic/channel names.
     The regex is ``^[.a-zA-Z0-9_-]{2,64}+(#ephemeral)?$``
 
@@ -38,7 +38,7 @@ def validate_topic_channel_name(name: str):
 
 
 @singledispatch
-def convert_to_bytes(value) -> bytes:
+def convert_to_bytes(value: Any) -> bytes:
     """Dispatch for convertible types.
 
     Allowed types: ``bytes``, ``bytearray``, ``str``, ``int``, ``float``,
@@ -66,7 +66,7 @@ def _(value: Union[bytes, bytearray]) -> bytes:
 
 
 @convert_to_bytes.register(str)
-def _(value: str) -> bytes:
+def _str_to_bytes(value: str) -> bytes:
     """Convert ``str`` to bytes"""
     return value.encode("utf-8")
 
@@ -74,31 +74,31 @@ def _(value: str) -> bytes:
 @convert_to_bytes.register(int)
 @convert_to_bytes.register(float)
 @convert_to_bytes.register(Decimal)
-def _(value: Union[int, float, Decimal]) -> bytes:
+def _numbers_to_bytes(value: Union[int, float, Decimal]) -> bytes:
     """Convert ``int``, ``float`` or ``Decimal`` to bytes"""
     return str(value).encode("utf-8")
 
 
 @convert_to_bytes.register(dict)
-def _(value: dict) -> bytes:
+def _dict_to_bytes(value: dict) -> bytes:
     """Convert ``dict`` to bytes"""
     return json.dumps(value, cls=JSONEncoder, separators=(",", ":")).encode("utf-8")
 
 
 @convert_to_bytes.register(Enum)
-def _(value: Enum) -> bytes:
+def _enum_to_bytes(value: Enum) -> bytes:
     """Convert ``enum`` to bytes"""
     return convert_to_bytes(value.name)
 
 
 @convert_to_bytes.register(datetime)
-def _(value: datetime) -> bytes:
+def _datetime_to_bytes(value: datetime) -> bytes:
     """Convert ``datetime`` to bytes"""
     return value.isoformat().encode("utf-8")
 
 
 @singledispatch
-def convert_to_str(value):
+def convert_to_str(value: Any) -> str:
     """Dispatch for convertible types.
 
     Allowed types: ``bytes``, ``bytearray``, ``str``, ``int``, ``float``,
@@ -119,19 +119,19 @@ def convert_to_str(value):
 
 
 @convert_to_str.register(str)
-def _(value: str) -> str:
+def _str_to_str(value: str) -> str:
     """Convert ``str`` to ``str``"""
     return value
 
 
 @convert_to_str.register(bytes)
-def _(value: bytes) -> str:
+def _bytes_to_str(value: bytes) -> str:
     """Convert ``bytes`` to ``str``"""
     return value.decode("utf-8")
 
 
 @convert_to_str.register(bytearray)
-def _(value: bytearray) -> str:
+def _bytearray_to_str(value: bytearray) -> str:
     """Convert ``bytearray`` to ``str``"""
     return bytes(value).decode("utf-8")
 
@@ -139,30 +139,32 @@ def _(value: bytearray) -> str:
 @convert_to_str.register(int)
 @convert_to_str.register(float)
 @convert_to_str.register(Decimal)
-def _(value: Union[int, float, Decimal]) -> str:
+def _numbers_to_str(value: Union[int, float, Decimal]) -> str:
     """Convert ``int``, ``float`` or ``Decimal`` to ``str``"""
     return str(value)
 
 
 @convert_to_str.register(dict)
-def _(value: dict) -> str:
+def _dict_to_str(value: dict) -> str:
     """Convert ``dict`` to JSON string"""
     return json.dumps(value)
 
 
 @convert_to_str.register(Enum)
-def _(value: Enum) -> str:
+def _enum_to_str(value: Enum) -> str:
     """Convert ``enum`` to str"""
     return convert_to_str(value.name)
 
 
 @convert_to_str.register(datetime)
-def _(value: datetime) -> str:
+def _datetime_to_str(value: datetime) -> str:
     """Convert ``datetime`` to bytes"""
     return value.isoformat()
 
 
-def get_logger(debug: bool = False, unique_name: str = None):
+def get_logger(
+    debug: bool = False, unique_name: Optional[str] = None,
+) -> logging.Logger:
     """Get the ansq logger.
 
     :params debug: Set up debug level.
