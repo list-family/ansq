254854d4b572f746f4d5a8aab1f6557fe4a29839
Albert Tugushev <albert@tugushev.ru>
2020-09-08 22:19:48 +0700

    Try to fix flaky test

--- tests/test_send_commands.py
+++ tests/test_send_commands.py
@@ -106,14 +106,14 @@ async def test_command_with_concurrently_closed_connection():
     nsq = await open_connection()
 
     async def wait_and_close():
-        await asyncio.sleep(0.1)
+        await asyncio.sleep(1)
         await nsq.close()
 
     async def blocking_wait_and_pub():
-        sleep(0.2)
+        sleep(2)
         await nsq.pub("test_topic", "test_message")
 
     with pytest.raises(ConnectionClosedError, match="^Connection is closed$"):
         await asyncio.wait_for(
-            asyncio.gather(wait_and_close(), blocking_wait_and_pub()), timeout=1,
+            asyncio.gather(wait_and_close(), blocking_wait_and_pub()), timeout=5,
         )
